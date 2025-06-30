7514410095a6bfddb995ac2c50b571166950e516
Shashank-kumar-Yadav <151130743+Shashank-kumar-Yadav@users.noreply.github.com>
2025-06-30 23:45:32 +0530

    .....

--- SQL Scripts/SQL Schema Pipeline/Load Data from CSV.sql
+++
@@ -1,8 +0,0 @@
-###  Step 2: Load Data from CSV
-
-LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/inventory_forecasting.csv'
-INTO TABLE InventoryForecasting
-FIELDS TERMINATED BY ',' 
-ENCLOSED BY '"'
-LINES TERMINATED BY '\n'
-IGNORE 1 ROWS;
